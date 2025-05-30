a38886d042baf2539b34c547548737af687037a7
rarcaleanu-at-eada <rarcaleanu@eada.edu>
2025-05-31 00:03:12 +0200

    feat: implemented test

---
+++ tests/test_transformer.py
@@ -0,0 +1,20 @@
+from src.transform import ClassSuportTransformer
+import pandas as pd
+
+
+def test_map_binary_column_to_int():
+    transformer = ClassSuportTransformer()
+    df = pd.DataFrame(
+        {
+            "Gender": ["Female", "Female", "Female", "Male"]
+        }
+    )
+
+    expected_df = pd.DataFrame(
+        {"Gender": [1, 1, 1, 0]}
+    )
+
+    transformed_df = transformer._map_binary_variables(df)
+
+    # Test the result against the expected DataFrame
+    pd.testing.assert_frame_equal(transformed_df, expected_df)
